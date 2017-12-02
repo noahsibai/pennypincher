d171a91c6e27485d603ee4b7fdbb81b77693db76
unknown <gsumm95@gmail.com>
2017-12-01 20:43:06 -0500

    Dashboard styling, percentage bar half-complete

--- app/src/main/java/pennypincher/cps496/cmich/edu/pennypincher/HomeFragment.java
+++ app/src/main/java/pennypincher/cps496/cmich/edu/pennypincher/HomeFragment.java
@@ -1,7 +1,6 @@
 package pennypincher.cps496.cmich.edu.pennypincher;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,26 +13,10 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.HorizontalBarChart;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
 
 import java.util.ArrayList;
 
-//import pennypincher.cps496.cmich.edu.pennypincher.R;
 
 
 /**
@@ -56,11 +39,6 @@ public class HomeFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    PieChart pieChart ;
-    ArrayList<Entry> entries ;
-    ArrayList<String> PieEntryLabels ;
-    PieDataSet pieDataSet ;
-    PieData pieData ;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -88,6 +66,12 @@ public class HomeFragment extends Fragment {
     DBHandler db;
     TableLayout tab;
 
+    RoundCornerProgressBar progress1;
+
+    double total_spent = 0;
+    BudgetDBHandler bdb;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -105,9 +89,15 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+//        HorizontalBarChart
+
+
+
+
 //  table
         Context mContext = v.getContext();
-        tab = (TableLayout) v.findViewById(R.id.Table);
+        tab = v.findViewById(R.id.Table);
         catArray = db.CategoryInfo();
         StringBuilder Sbuilder = new StringBuilder();
         for(int i = 0; i < catArray.size();i++){
@@ -116,6 +106,7 @@ public class HomeFragment extends Fragment {
         }
 
         for(int i = 0; i < catArray.size(); i++){
+            total_spent = total_spent + (catArray.get(i).getAmount());
             TableRow tr = new TableRow(mContext);
             TextView tv = new TextView(mContext);
             TextView tv2 = new TextView(mContext);
@@ -127,138 +118,22 @@ public class HomeFragment extends Fragment {
             tr.addView(tv2);
             tab.addView(tr);
         }
-//  Horizontal Barchart
-        HorizontalBarChart chart = (HorizontalBarChart) v.findViewById(R.id.chart);
 
-        BarDataSet set1;
-        set1 = new BarDataSet(getDataSet(), "The year 2017");
+        //  PrgBar
+        bdb = new BudgetDBHandler(v.getContext(), "we", null, 1);
 
-        set1.setColors(Color.parseColor("#26cc42"));
-
-        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
-        dataSets.add(set1);
-
-        BarData data = new BarData(dataSets);
-
-        // hide Y-axis
-        YAxis left = chart.getAxisLeft();
-        left.setDrawLabels(false);
-
-        chart.getAxisRight().setAxisMinValue(0f);
-        chart.getAxisRight().setAxisMaxValue(100f);
-        chart.getXAxis().setDrawGridLines(false);
-        chart.getAxisLeft().setDrawGridLines(false);
-        chart.getAxisRight().setDrawGridLines(false);
+        Budget t = bdb.GetAllRecords();
+        progress1 = v.findViewById(R.id.progress_1);
+//        get the percentage
+        progress1.setMax((float)(t.GetAmount()));
+        progress1.setProgress((float)(total_spent)-5);
+        progress1.setSecondaryProgress((float)(total_spent));
 
 
-        // custom X-axis labels
-        String[] values = new String[] { " ", " "};
-        XAxis xAxis = chart.getXAxis();
-        xAxis.setValueFormatter(new MyXAxisValueFormatter(values));
-
-        chart.setData(data);
-
-        // custom description
-        Description description = new Description();
-        description.setText(" ");
-        chart.setDescription(description);
-
-        // hide legend
-        chart.getLegend().setEnabled(false);
-
-        chart.animateXY(1000, 1000);
-        chart.invalidate();
-
-// bar/pie chart
-//        BarChart Bchart = (BarChart) v.findViewById(R.id.chart1);
-//
-//        BarData Bdata = new BarData(getBDataSet());
-//        Bchart.setData(Bdata);
-//        Bchart.setDescription(description);
-//        Bchart.animateXY(2000, 2000);
-//        Bchart.invalidate();
 
         return v;
     }
-//    private ArrayList<String> BgetXAxisValues() {
-//        ArrayList<String> xAxis = new ArrayList<>();
-//        xAxis.add("JAN");
-//        xAxis.add("FEB");
-//        xAxis.add("MAR");
-//        xAxis.add("APR");
-//        xAxis.add("MAY");
-//        xAxis.add("JUN");
-//        return xAxis;
-//    }
-//
-//    private ArrayList<BarDataSet> getBDataSet() {
-//
-//        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
-//        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
-//        valueSet1.add(v1e1);
-//        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
-//        valueSet1.add(v1e2);
-//        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
-//        valueSet1.add(v1e3);
-//        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
-//        valueSet1.add(v1e4);
-//        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
-//        valueSet1.add(v1e5);
-//        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
-//        valueSet1.add(v1e6);
-//
-//        ArrayList<BarEntry> valueSet2 = new ArrayList<>();
-//        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
-//        valueSet2.add(v2e1);
-//        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
-//        valueSet2.add(v2e2);
-//        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
-//        valueSet2.add(v2e3);
-//        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
-//        valueSet2.add(v2e4);
-//        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
-//        valueSet2.add(v2e5);
-//        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
-//        valueSet2.add(v2e6);
-//
-//        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
-//        barDataSet1.setColor(Color.rgb(0, 155, 0));
-//        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
-//        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
-//
-//        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
-//        dataSets.add(barDataSet1);
-//        dataSets.add(barDataSet2);
-//        return dataSets;
-//    }
 
-
-    public class MyXAxisValueFormatter implements IAxisValueFormatter {
-
-        private String[] mValues;
-
-        public MyXAxisValueFormatter(String[] values) {
-            this.mValues = values;
-        }
-
-        @Override
-        public String getFormattedValue(float value, AxisBase axis) {
-            return mValues[(int) value];
-        }
-
-    }
-
-    private ArrayList<BarEntry> getDataSet() {
-
-        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
-
-        BarEntry v1e2 = new BarEntry(0, 75);
-        valueSet1.add(v1e2);
-
-
-        Log.d("dset", valueSet1.toString());
-        return valueSet1;
-    }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
